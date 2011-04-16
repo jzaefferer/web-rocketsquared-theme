204c34d832600b0e8469d3ad853fd3ee8ee80864
jzaefferer <joern.zaefferer@gmail.com>
2011-04-16 14:30:55 +0200

    Add titles to footer icons

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
             <?php wp_nav_menu( array( 'menu_class' => 'footerNavigation', 'sort_column' => 'menu_order', 'container_class' => 'nav', 'theme_location' => 'primary-menu' ) ); ?>
             <a class="contactLink" href="mailto:joern.zaefferer@googlemail.com">Contact</a>
             <ul id="footerSocial">
-            	<li><a class="twitter-icon" href="http://twitter.com/bassistance">Twitter</a></li>
-                <li><a class="github-icon" href="http://github.com/jzaefferer">GitHub</a></li>
-                <li><a class="linkedin-icon" href="http://www.linkedin.com/in/joernzaefferer">LinkedIn</a></li>
+                <li><a class="twitter-icon" title="@bassistance on Twitter" href="http://twitter.com/bassistance">Twitter</a></li>
+                <li><a class="github-icon" title="jzaefferer on GitHub" href="http://github.com/jzaefferer">GitHub</a></li>
+                <li><a class="linkedin-icon" title="joernzaefferer on LinkedIn" href="http://www.linkedin.com/in/joernzaefferer">LinkedIn</a></li>
             </ul>
         </footer>
     </div>
