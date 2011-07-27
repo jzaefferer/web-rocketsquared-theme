619b3f51c507e6c43392eace999174d4a030a419
Jörn Zaefferer <joern.zaefferer@gmail.com>
2011-07-27 12:19:53 +0200

    Improve post footer, show date, remove author. Remove placeholders for comments, not used anyway.

--- single.php
+++ single.php
@@ -1,34 +1,31 @@
 <?php get_header(); ?>
 
-    <div id="content">
-        <div class="posts">
-        <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
+  <div id="content">
+    <div class="posts">
+    <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
 
-        <div class="post">
-        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-            <div class="entry">
-                <?php the_post_thumbnail(); ?>
-                <?php the_content(); ?>
+    <div class="post">
+    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+    <?php edit_post_link('Edit', ' ', ''); ?>
+    <div class="entry">
+      <?php the_post_thumbnail(); ?>
+      <?php the_content(); ?>
 
-                <p class="postmetadata">
-                <?php _e('Filed under&#58;'); ?> <?php the_category(', ') ?> <?php _e('by'); ?> <?php  the_author(); ?><br />
-                <?php if(comments_open()) { comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); } ?> <?php edit_post_link('Edit', ' &#124; ', ''); ?>
-                </p>
-
-            </div>
-
-            <?php if(comments_open()) : ?>
-                <div class="comments-template">
-                    <?php comments_template(); ?>
-                </div>
-            <?php endif; ?>
+      <p class="postmetadata">
+       <?php echo get_the_date() ?>, <?php _e('tagged with'); ?> <?php the_category(', ') ?>
+      </p>
     </div>
+    <div id="nav-below" class="navigation">
+      <div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentyten' ) . '</span> %title' ); ?></div>
+      <div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentyten' ) . '</span>' ); ?></div>
+    </div>
+  </div>
 
 <?php endwhile; ?>
-        </div>
-            <?php endif; ?>
-        <?php get_sidebar(); ?>
-        <div style="clear:both"></div>
+  </div>
+    <?php endif; ?>
+  <?php get_sidebar(); ?>
+  <div style="clear:both"></div>
 </div>
 
 <?php get_sidebar(); ?>
